ffd221af3f4a919681420534bb92e4586fd3a599
alexghiurau <alexghiurau003@gmail.com>
2021-11-16 13:36:50 +0000

    optimisation changes

--- anagrams.py
+++ anagrams.py
@@ -6,52 +6,41 @@ import sys
 # By Alex Ghiurau
 # -----------------------------------------------------------
 
-"""groupAnagrams - Function to group anagrams from a given list of words
-   Args:
-        words (array): array of words taken from user provided file
-   Returns:
-        anagrams (array): an array words where anagrams are grouped"""
-def groupAnagrams(words):
-    # a list to store anagrams
-    anagrams = []
- 
-    # return if no words are found
-    if not words:
-        return anagrams
- 
-    # sort the characters of each word in alphabetical order
-    sortedChars = [''.join(sorted(word)) for word in words]
- 
-    # create a dictionary where the key is each sorted chars instance,
-    # and the value is a list of indices where they are found
+def sortAnagrams(lines):
+    # create a dictionary, key is the sorted word, value is a 
+    # list of words (including any anagrams)
     dictionary = {}
-    for index, sortedWord in enumerate(sortedChars):
-        dictionary.setdefault(sortedWord, []).append(index)
- 
-    # use the dictionary to map out the array, grouping anagrams
-    # using using tuples and the indices from the dictionary.
-    for index in dictionary.values():
-        collection = tuple(words[i] for i in index)
-        anagrams.append(collection)
+
+    for line in lines:
+        # sort each word's characters alphabetically
+        sortedLine = ''.join(sorted(line))
+        if sortedLine in dictionary:
+            dictionary[sortedLine].append(line)
+        else:
+            dictionary[sortedLine] = [line]
+
+    # use dictionary to map out an array of words, for writing to file
+    anagrams = [dictionary[k] for k in dictionary]
+
+    writeToFile(anagrams)
+
+# read from user-specified file passed as parameter
+def readFromFile():
     
-    return anagrams
+    with open(sys.argv[1], 'r') as reader:
+        # create array with each word from file
+        lines = [line.strip() for line in reader]
 
-# create array to store the words from the provided file, which is then 
-# passed to groupAnagrams()
-wordsFromFile = []
+        # remove duplicate lines
+        lines = set(lines)
+        
+        sortAnagrams(lines)
 
-# using 'with open' to ensure the file is opened/closed correctly
-# this method ensures we do not open the whole file at once, but line by line
-# which is important considering the limited memory assumption
-# file name is passed as args
-with open(sys.argv[1], 'r') as reader:
-    for line in reader:
-        wordsFromFile.append(line.rstrip())
- 
-anagrams = groupAnagrams(wordsFromFile)
+# write to the file 'sortedWords.txt', replacing existing data (if any)
+def writeToFile(anagrams):
+    with open('sortedWords.txt', 'w') as writer:
+        for anagram in anagrams:
+            writer.write(' '.join([str(s) for s in list(anagram)]) + '\n')
 
-# similar to above but we write in a separate file in case the original must stay intact
-# if this file does not exist, it gets created otherwise all text is replaced by new output
-with open('sortedWords.txt', 'w') as writer:
-    for anagram in anagrams:
-        writer.write(' '.join([str(s) for s in list(anagram)]) + '\n')
+# when program loads, read from specified file
+readFromFile()
